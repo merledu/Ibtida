a4434e73b8795fc4ba6d1d7e3eae6f0c259d4ee8
Hadir Khan <hadirkhan10@gmail.com>
2020-11-30 13:47:50 +0500

    Changed the dccm mask for 64 words memory

--- src/main/scala/ibtida_soc/IbtidaConfig.scala
+++ src/main/scala/ibtida_soc/IbtidaConfig.scala
@@ -10,7 +10,7 @@ object AddressMap {
   val ADDR_MASK_GPIO  = "h00000fff".U(32.W)
   val ADDR_SPACE_DCCM = "h10000000".U(32.W)
   //val ADDR_MASK_DCCM  = "h00003fff".U(32.W) // mask for 64 KB ram
-  val ADDR_MASK_DCCM  = "h000000ff".U(32.W)   // mask for 1KB ram
+  val ADDR_MASK_DCCM  = "h000000fc".U(32.W)   // mask for 64 words ram
 }
 
 object TL_Peripherals {
